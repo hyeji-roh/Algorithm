33103abbd2e51b89fe53997687d318eb5cd6dfa4
노혜지 <raonlight_sophie@naver.com>
2026-02-23 20:11:23 +0900

    [Silver IV] Title: 스택, Time: 340 ms, Memory: 112032 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,20 @@
+N=int(input())
+stack = []
+
+for _ in range(N):
+    command = input().split()
+
+    if command[0]=="push":
+        stack.append(command[1])
+    elif command[0]=="pop":
+        if len(stack)!=0:
+            print(stack[-1])
+            stack.pop()
+        else:
+            print(-1)
+    elif command[0]=="size":
+        print(len(stack))
+    elif command[0]=="empty":
+        print(1) if len(stack) == 0 else print(0)
+    elif command[0]=="top":
+        print(stack[-1]) if len(stack)!=0 else print(-1)
