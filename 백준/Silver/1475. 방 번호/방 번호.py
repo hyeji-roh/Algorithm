812dea63335f4a69bda337c7c7b5d9698ee744e9
노혜지 <raonlight_sophie@naver.com>
2026-03-17 19:15:42 +0900

    [Silver V] Title: 방 번호, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -5,11 +5,10 @@ for i in n:
     arr[int(i)] += 1
 
 six_nine = arr[6]+arr[9]
-if (six_nine)%2 == 0:
-    arr[6] = six_nine//2
-    arr[9] = six_nine // 2
+
+if six_nine % 2 == 0:
+    arr[6] = arr[9] = six_nine//2
 else:
-    arr[6] = (six_nine//2)+1
-    arr[9] = (six_nine//2)+1
+    arr[6] = arr[9] = (six_nine//2)+1
 
 print(max(arr))
