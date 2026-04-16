727b0e8f4899e81073222b177e3ed5dc90a3b341
노혜지 <raonlight_sophie@naver.com>
2026-04-16 14:25:44 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, letter):
+    answer = ''
+    for word in my_string:
+        if word != letter:
+            answer += word
+        else: 
+            continue
+    return answer
