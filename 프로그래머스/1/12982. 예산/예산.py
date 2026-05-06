4443be91740e8abd1ecf0bfc0ae7ffeef446ed81
노혜지 <raonlight_sophie@naver.com>
2026-05-06 20:48:52 +0900

    [level 1] Title: 예산, Time: 0.09 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,14 +1,13 @@
 def solution(d, budget):
-    d = sorted(d)
-    result = 0
-    tmp = 0
-    
-    for value in d:
-        if tmp < budget:
-            tmp += value
-            if tmp <= budget:
-                result += 1
-            else:
-                break
-    return result
-    
+    d.sort() 
+
+    start = 0
+    end = len(d)
+
+    for _ in range(len(d)):
+        if sum(d[start:end]) <= budget:
+            return end - start
+        else:
+            end -= 1
+
+    return 0 
